2ca1cadd8084e4b8633537074ffe23d47edb210d
SaloniPawar <52480984+SaloniPawar@users.noreply.github.com>
2024-03-21 17:57:54 +0530

    Updated

--- provider.tf
+++ provider.tf
@@ -19,21 +19,3 @@ provider "aws" {
 
 }
 
-# terraform {
-#   backend "s3" {
-#       bucket = "terrafom-bucket"
-#       key = "path/to/key"
-#       region = "us-east-1"
-#   }
-
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "4.22.1"
-#     }
-#   }
-
-#   provider "aws" {
-#     region = "us-east-1"
-#   }
-# }
